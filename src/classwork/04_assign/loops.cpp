ade5881fdcf4a8ab1a04f096622f8eb1db29a788
JustinBrewer22 <justin.brewer2@g.austincc.edu>
2021-05-13 08:27:57 -0500

    HW4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,14 @@
 //cpp
+#include "loops.h"
+
+int factorial(int num)
+{
+    int a = 1;
+    
+    while(num > 0)
+    {
+        a *= num;
+        num -= 1;
+    }
+    return a;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include "loops.h"
+#include <iostream>
 //write using statements for cin and cout
-
+using namespace std;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +11,23 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	char repeat;
+	int num;
+	do{
+		do{
+			cout << "Please enter a number 1 to 10 : ";
+			cin >> num;
+
+		} while(!(num >= 1 && num <= 10));
+
+		
+		cout << "The factorial of "<< num <<" is: " << factorial(num) << "\n";
+
+		cout << "Would you like to calculate another factorial? Please enter: (Y for Yes) or (N for No): \n";
+		cin >> repeat;
+		} 
+	
+	while(repeat == 'y' || repeat == 'Y');
 
 	return 0;
 }

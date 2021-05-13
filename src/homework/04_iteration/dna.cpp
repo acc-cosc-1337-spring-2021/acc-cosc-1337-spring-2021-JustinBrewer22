caf6aec9544ba0163465d649bcdfa9c2fc1925b8
JustinBrewer22 <justin.brewer2@g.austincc.edu>
2021-05-13 11:05:22 -0500

    HW 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,9 @@
 #include "dna.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,7 +11,20 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+    double gc_count;
 
+    for(int count = 0; count <= dna.length(); count = count + 1)
+    {
+        if (dna[count] == 'G' || dna[count] == 'C')
+        {
+            gc_count += 1;
+        }
+    }
+
+    return (gc_count / dna.length());
+}
 
 
 
@@ -14,7 +32,17 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna)
+{
+    string reverse;
 
+    for(int count = dna.length() - 1; count >=0; count = count - 1)
+    {
+        reverse.push_back(dna[count]);
+    }
+    
+    return reverse;
+}
 
 
 /*
@@ -27,4 +55,29 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna)
+{
+    string dna_complement = get_reverse_string(dna);
+
+    for (int i = 0; i < dna_complement.length(); i = i + 1)
+    {
+        if (dna_complement[i] == 'A')
+        {
+            dna_complement[i] = 'T';
+        }
+        else if (dna_complement[i] == 'T')
+        {
+            dna_complement[i] = 'A';
+        }
+        else if (dna_complement[i] == 'C')
+        {
+            dna_complement[i] = 'G';
+        }
+        else if (dna_complement[i] == 'G')
+        {
+            dna_complement[i] = 'C';
+        }
+    }
+    return dna_complement;
+}
 
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,36 @@ user enters a y or Y.
 */
 int main() 
 {
+	char rerun;
+	char input;
+	string dna;
+
+	do
+	{
+		cout << "Please select '1' for Get GC Content, or '2' for Get DNA Complement: \n";
+		cin>>input;
+		switch (input)
+		{
+			case '1':
+				cout << "Please enter a DNA string: \n";
+				cin >> dna;
+				cout << "The GC content is: " << get_gc_content(dna) << "\n";
+				break;
+			
+			case '2':
+				cout << "Please enter a DNA string: \n";
+				cin >> dna;
+				cout << "The DNA Complement is: " << get_dna_complement(dna) << "\n";
+				break;
+			
+			default:
+				cout << "You did not select '1' or '2'.";
+				break;
+		}
+
+		cout << "Enter 'Y' to quit or enter any other key to continue using the program. \n";
+		cin >> rerun;
+	} 
+	while (!(rerun == 'y' || rerun == 'Y'));
 	return 0;
 }
